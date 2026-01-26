75759149eb821bf3e43315f600e553493eb939f6
Andrey Gaponik <andreygaponik@gmail.com>
2026-01-26 17:15:31 +0300

    refactor: Implement FSD structure and path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Header } from './widgets/Header/Header';
+
 function App() {
 	return (
 		<div>
 			<h1>React Music Player</h1>
+			<Header />
 		</div>
 	);
 }
--- src/widgets/Header/Header.test.tsx
+++ src/widgets/Header/Header.test.tsx
@@ -4,13 +4,10 @@ import { Header } from './Header';
 
 describe('Header component', () => {
 	it('should render the project title', () => {
-		// 1. Рендерим компонент в виртуальном окружении
 		render(<Header />);
 
-		// 2. Ищем на "экране" элемент с текстом "React Music Player" (без учета регистра)
 		const titleElement = screen.getByText(/react music player/i);
 
-		// 3. Ожидаем, что такой элемент существует в документе
 		expect(titleElement).toBeInTheDocument();
 	});
 });
